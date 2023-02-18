70f012ddb3f805de22f3159f503079ab00980cc5
atlasmac <mckinleyatlas@gmail.com>
2023-02-18 16:04:09 -0700

    reduce image size

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import profile from '../images/raeleahProfile.png'
+import profile from '../images/raeleahProfile.jpg'
 
 const About = () => {
   return (
     <div className='div' id='about'>
       <div className='inner-div'>
-      <h2>Hi, I’m Raeleah!</h2>
+        <h2>Hi, I’m Raeleah!</h2>
         <div className='about-div'>
-        
+
           <div className='img-div'>
             <img className='bio-img' src={profile} alt="profile" />
           </div>
           <div className='bio'>
-            
+
             <p>I’m a Wisconsin native and Montana resident with a passion for connecting with kids and their families. I LOVE problem solving with parents on creative ways to support their child’s mental health and improve their regulation and behavior at home and in the community. </p>
             <p>Inspired by my own experiences, I am excited to connect with more children and families in a collaborative effort to support pediatric mental health. I have my masters in occupational therapy with experience working in pediatric home health, schools and clinic settings. </p>
           </div>
