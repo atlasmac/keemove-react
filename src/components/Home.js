4cd4324276a56137706f99934afe2fb363e1d406
atlasmac <mckinleyatlas@gmail.com>
2024-05-16 10:07:53 -0600

    remove uncomented code

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,6 @@ const Home = () => {
     <div className='home-div' id='home'>
       <div>
         <h1>Boosting Your Child&#39;s Confidence</h1>
-        {/* <HashLink to='/' smooth>
-          Contact Us
-        </HashLink> */}
         <HashLink to='/#book' smooth>
           Book With Us
         </HashLink>
