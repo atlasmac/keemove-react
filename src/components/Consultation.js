dc373f030b9a82b77fbf3aa829e497618bdf6e2f
atlasmac <mckinleyatlas@gmail.com>
2024-05-17 17:44:26 -0600

    remove pediatric consultation

--- src/components/Consultation.js
+++ src/components/Consultation.js
@@ -1,29 +1,14 @@
-import React from 'react'
-import CalendlyButton from './CalendlyButton'
+import React from "react";
+import CalendlyButton from "./CalendlyButton";
 
 const Consultation = () => {
   return (
-    <div className='div' id='book'>
-      <div className='inner-div consultation'>
-        <h2>Pediatric Consultation</h2>
-        <div className='con-div'>
-          <p>KeeMove was established to provide individualized support for families navigating difficult seasons. Raeleah uses her specialized training in pediatric development, behavior management and regulation practices to problem solve with families. Her model focuses on:</p>
-          <ul>
-            <li>Strength-based approach for parent and child</li>
-            <li>Neurodiversity affirming - understanding and appreciating individual differences</li>
-            <li>Mind-body connection</li>
-            <li>Parent-child connection</li>
-            <li>Supports sensory health</li>
-            <li>Applies principles of the Universal Design for Learning</li>
-            <li>Thinks outside the box to find tools and strategies to use at-home and in the community</li>
-            <li>Empowers children and families with education and resources that benefit the whole family</li>
-          </ul>
-          <p>Book a free initial consultation below.</p>
-        </div>
+    <div className="div" id="book">
+      <div className="inner-div consultation">
         <CalendlyButton />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Consultation
+export default Consultation;
